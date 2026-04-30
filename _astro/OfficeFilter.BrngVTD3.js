d9be7996fdfb77b2c4c69093843d9afca97db262
maxbolgarin <maxbolgarin@users.noreply.github.com>
2026-04-30 12:43:09 +0000

    deploy: 5f0cb410e084e3e7f0009211aea6ff730fdd6c4d

---
+++ _astro/OfficeFilter.BrngVTD3.js
@@ -0,0 +1,1 @@
+import{default as p}from"./OfficeFilter.19oFXIVH.js";import"./withBase.DfNQnp7z.js";import"./index.DiEladB3.js";export{p as default};
--- _astro/OfficeFilter.q7BecX5B.js
+++
@@ -1,1 +0,0 @@
-import{default as p}from"./OfficeFilter.DpV-QUL2.js";import"./withBase.DfNQnp7z.js";import"./index.DiEladB3.js";export{p as default};
